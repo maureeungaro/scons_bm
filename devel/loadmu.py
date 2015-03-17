f80d41335f5d6678f04b42deab4c8ba026e74128
maureeungaro <maurizio.ungaro@cnu.edu>
2015-03-17 08:24:44 -0400

    added binning library

--- devel/loadmu.py
+++ devel/loadmu.py
@@ -11,9 +11,10 @@ def loadmu(env) :
 	env.Append(CXXFLAGS = env['INCPREFIX'] + MU + '/v4')
 	env.Append(CXXFLAGS = env['INCPREFIX'] + MU + '/src')
 	env.Append(CXXFLAGS = env['INCPREFIX'] + MU + '/bos2mu')
+	env.Append(CXXFLAGS = env['INCPREFIX'] + MU + '/binning')
 
 	libs = [
-	        'CLAS_Event', 'V4', 'utilities', 'CLAS_Event'
+	        'CLAS_Event', 'V4', 'utilities', 'CLAS_Event', 'bin'
 	       ]
 
 	mulibs = []
