aa87a4a0fe42b654655e73225778b31b17baf6be
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2016-07-11 13:29:14 -0400

    checking library existance before adding it to the list of libraries

--- devel/loadmlibrary.py
+++ devel/loadmlibrary.py
@@ -1,6 +1,8 @@
 from util import check_if_dir_exist
 import os
 
+from SCons.Script import *
+
 # MLIBRARY already has the version in it
 
 def loadmlibrary(env) :
@@ -9,15 +11,18 @@ def loadmlibrary(env) :
 	check_if_dir_exist('MLIBRARY')
 
 	env.Append(CPPPATH = [MLIBRARY + '/options'])
-
+	conf = Configure(env)
 	libs = [
 			  'options',
 			  'translationTable'
 			  ]
 
+	# only load library if it exists
 	c12libs = []
 	for lib in libs:
-		c12libs.append(lib)
+		filename = [MLIBRARY + '/lib/' + lib]
+		if conf.CheckLib(lib):
+			c12libs.append(lib)
 
 	env.Append(LIBPATH = [MLIBRARY + '/lib'])
 	env.Append(LIBS = c12libs)
