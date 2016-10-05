e4748ec76f6811c1e826b4c5d1b64559459f71a5
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2016-10-05 13:58:41 -0400

    release 1.4 of the scons build system

---
+++ 1.4/loadcuda.py
@@ -0,0 +1,68 @@
+from util import check_if_dir_exist
+import os
+from SCons.Builder import *
+
+
+def loadcuda(env) :
+	OSENV  = os.environ
+	CUDA_HOME = OSENV['CUDA_HOME']
+	CUDA_LIB  = OSENV['CUDA_LIB']
+	check_if_dir_exist('CUDA_HOME')
+
+	# Cuda compiler options
+	env.Append(CUDA_FLAGS = env['INCPREFIX'] + CUDA_HOME + '/include ')
+	env.Append(CUDA_FLAGS = env['INCPREFIX'] + CUDA_HOME + '/sdk/C/common/inc ')
+	env.Append(CUDA_FLAGS = '--compiler-options ')
+	if env['PLATFORM'] == 'posix':
+		env.Append(CUDA_FLAGS='-DUNIX')
+	if env['CUDA_EMU'] == "1":
+		env.Append(CUDA_FLAGS=' -deviceemu ')
+			
+	if env['SHOWENV'] == "1":
+		print "Loading CUDA software from ", CUDA_HOME, CUDA_FLAGS
+
+	
+	cuda_bld = Builder(action = '/usr/local/cuda/bin/nvcc  $CUDA_FLAGS -o $TARGETS -c $SOURCES',
+										 suffix = '.cuo')
+	env.Append(BUILDERS = {'CUDABuild' : cuda_bld})
+
+
+	# Now normal source code options, libraries
+	env.Append(CXXFLAGS = env['INCPREFIX'] + CUDA_HOME + '/include')
+	env.Append(CXXFLAGS = env['INCPREFIX'] + CUDA_HOME + '/sdk/C/common/inc')
+	
+	libs = [
+					'cudart', 'paramgl', 'rendercheckgl',  'GLEW_x86_64', 'cutil'
+	       ]
+	if env['CUDA_EMU'] == "1":
+		libs.append('cudpp64_emu')
+	else:
+		libs.append('cudpp64')
+		
+	
+	cudalibs = []
+	for lib in libs:
+		cudalibs.append(lib)
+
+
+	env.Append(LIBPATH = [CUDA_LIB])
+	env.Append(LIBPATH = [CUDA_HOME + '/sdk/C/lib'])
+	env.Append(LIBS = cudalibs)
+
+	if env['PLATFORM'] == 'posix':
+		env.Append(LIBPATH = [CUDA_HOME + '/sdk/C/common/lib/linux'])
+		env.Append(CXXFLAGS='-DUNIX')
+		cudalibs.append('GL')
+		cudalibs.append('GLU')
+		cudalibs.append('Xmu')
+		cudalibs.append('X11')
+		cudalibs.append('Xi')
+		cudalibs.append('Xmu')
+		cudalibs.append('glut')
+		env.Append(LIBS = cudalibs)
+		if env['CUDA_EMU'] == "1":
+			env.Append(CXXFLAGS='-D__DEVICE_EMULATION__')
+			
+
+
+
---
+++ 1.4/loadqt.py
@@ -0,0 +1,40 @@
+import os
+
+def loadqt(env) :
+
+	OSENV = os.environ
+	QTDIR = OSENV['QTDIR']
+
+	# QtCore, QtGui, QtWidgets, QtOpenGL, QtPrintSupport are enabled in geant4
+	# However when compiling non-geant4 apps they need to be included
+	qtincs = [];
+	qtlibs = [];
+	qtModules = ["QtCore", "QtGui", "QtWidgets", "QtOpenGL", "QtXml", "QtSql"]
+
+	for mod in qtModules:
+		qtincs.append(" -I" + QTDIR + "/lib/" + mod + ".framework/Headers")
+		qtlibs.append(' -framework ' + mod )
+	# joining all together
+	qtcppf  = ''.join(qtincs)
+	qtlinkf = ''.join(qtlibs)
+
+	# The location of the headers is different in Linux and Darwin (oh why oh why)
+	if env['PLATFORM'] == 'darwin':
+		env.Append(CPPFLAGS  = "-F" + QTDIR + "/lib" )
+		env.Append(CPPFLAGS = qtcppf )
+		env.Append(LINKFLAGS = "-F" + QTDIR + "/lib" )
+		env.Append(LINKFLAGS = qtlinkf)
+		# added because starting with qt 5.5: the prebuilt 5.5.0 binaries from Qt
+		# use @rpath for mac instead @excutable_path or an absolute path.
+		# For Linux this is not necessary
+		env.Append(LINKFLAGS  = "-rpath " + QTDIR + "/lib" )
+
+	else:
+		env['ENV']['PKG_CONFIG_PATH'] = os.environ['PKG_CONFIG_PATH']
+		env.EnableQt5Modules(qtModules)
+		env.Append(CPPFLAGS = ' -fPIC')
+
+	# Qt existance is already checked
+	if env['SHOWENV'] == "1":
+		print "\n > Loading QT5 software from ",  QTDIR , "for ", env['PLATFORM']
+		print "   Qt Modules: ",  qtModules
