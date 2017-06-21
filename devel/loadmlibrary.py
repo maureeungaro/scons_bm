6069f72367e5bc4b5fa59c3a1ec6bc4f08f5d2aa
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-06-21 12:33:24 -0400

    case sensitivity not caught on mac

--- devel/loadmlibrary.py
+++ devel/loadmlibrary.py
@@ -11,7 +11,7 @@ def loadmlibrary(env) :
 	check_if_dir_exist('MLIBRARY')
 
 	## includes
-	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gfactory', 'gVolume', 'g4volume', 'g4display']
+	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gfactory', 'gvolume', 'g4volume', 'g4display']
 	mincludes = []
 	for minc in mincs:
 		thisInc =  MLIBRARY + '/' + minc
