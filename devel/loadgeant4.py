8c14f13e1b8354922d1bb316629e3558dafb845d
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-09-18 10:23:00 -0400

    by default do not use G4 MT yet

--- devel/loadgeant4.py
+++ devel/loadgeant4.py
@@ -15,7 +15,7 @@ def loadgeant4(env) :
 	env.Append(CXXFLAGS='-DG4UI_USE_QT')
 	env.Append(CXXFLAGS='-DG4VIS_USE_OPENGLQT')
 	env.Append(CXXFLAGS='-DG4USE_STD11')
-	env.Append(CXXFLAGS='-DG4MULTITHREADED')
+	# env.Append(CXXFLAGS='-DG4MULTITHREADED')
 
 	## includes
 	# Stripping all entries that are not geant4 related
