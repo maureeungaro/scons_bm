d6d753e81d092281c9d6cce67ea13c1eaebd2ed3
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-02 10:15:01 -0400

    - added gdata mlibrary in header search path

--- devel/loadmlibrary.py
+++ devel/loadmlibrary.py
@@ -11,7 +11,7 @@ def loadmlibrary(env) :
 	check_if_dir_exist('MLIBRARY')
 
 	## includes
-	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gfactory', 'gdynamic', 'gtouchable', 'gvolume', 'g4volume', 'g4display', 'ghit']
+	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gfactory', 'gdynamic', 'gtouchable', 'gvolume', 'g4volume', 'g4display', 'ghit', 'gdata']
 	mincludes = []
 	for minc in mincs:
 		thisInc =  MLIBRARY + '/' + minc
