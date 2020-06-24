eda50bba90b4ed029c1aabb22bf79f43345c81ae
Mauri <maurizio.ungaro@cnu.edu>
2020-06-24 12:20:18 -0400

    Update loadmlibrary.py

--- loadmlibrary.py
+++ loadmlibrary.py
@@ -11,7 +11,7 @@ def loadmlibrary(env) :
 	check_if_dir_exist('MLIBRARY')
 
 	## includes
-	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gdynamic', , 'gvolume', 'g4volume', 'g4display',  'gmedia']
+	mincs = ['options', 'translationTable', 'splash', 'gruns', 'textProgressBar', 'gstring', 'frequencySyncSignal', 'qtButtonsWidget', 'gdynamic', 'gvolume', 'g4volume', 'g4display',  'gmedia']
 	mincludes = []
 	for minc in mincs:
 		thisInc =  MLIBRARY + '/' + minc
