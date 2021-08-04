7a06dcee618d4ee439267b9a35252f4923472221
maureeungaro <maureeungaro@gmail.com>
2021-08-04 11:25:31 -0400

    added g4display

--- loadglibrary.py
+++ loadglibrary.py
@@ -11,7 +11,7 @@ def loadglibrary(env, OSENV) :
 
 	## includes
 	gincs1 = ['guts', 'goptions', 'gsplash', 'gdata', 'gfactory', 'gtouchable', 'ghit', 'gdynamicdigitization', 'gsystem', 'g4system']
-	gincs2 = ['gstreamer', 'textProgressBar', 'eventDispenser', 'gQtButtonsWidget']
+	gincs2 = ['gstreamer', 'textProgressBar', 'eventDispenser', 'gQtButtonsWidget', 'g4display']
 
 	gincs = gincs1 + gincs2
 
