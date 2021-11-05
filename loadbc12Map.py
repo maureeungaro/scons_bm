b37cab069b65c607cc7dd371f40ca4f08f33b76a
maureeungaro <maureeungaro@gmail.com>
2021-11-05 10:51:31 -0400

    added loadbc12Map.py

---
+++ loadbc12Map.py
@@ -0,0 +1,25 @@
+from util import check_if_dir_exist
+
+def loadevio(env, OSENV) :
+	C12BFIELDS = OSENV['C12BFIELDS']
+	check_if_dir_exist('C12BFIELDS', EVIO)
+
+	## includes
+	incs = [C12BFIELDS + 'includes']
+	env.Append(CPPPATH = incs)
+
+
+	## library paths
+	ldir = [C12BFIELDS + '/lib']
+	env.Append(LIBPATH = ldir)
+			
+	## libraries
+	libs = ['cMag']
+	env.Append(LIBS = libs)
+
+	# print environment if requested
+	if env['SHOWENV'] == "1":
+		print ("\n > Loading C12BFIELDS software from ", C12BFIELDS)
+		print ("   C12BFIELDS include flags: ",  incs)
+		print ("   C12BFIELDS libraries path: ", ldir)
+		print ("   C12BFIELDS libraries: ",      libs)
