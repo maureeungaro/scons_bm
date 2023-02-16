270e055dd445c2ec5fa426d3cd05dbeb487adc30
maureeungaro <maureeungaro@gmail.com>
2023-02-16 14:16:42 -0500

    tagging 1.11 with G4PTL - specific for g4 10.7

--- loadgeant4.py
+++ loadgeant4.py
@@ -6,9 +6,7 @@ def loadgeant4(env, OSENV) :
 	check_if_dir_exist('G4INSTALL', G4INSTALL)
 
 	# Geant 4 additional compiler options
-	# Notive: DG4_STORE_TRAJECTORY is on by default G4 in MT support
 	env.Append(CXXFLAGS='-DG4OPTIMISE')
-	# env.Append(CXXFLAGS='-DG4_STORE_TRAJECTORY')
 
 	## includes
 	# Stripping all entries that are not geant4 related
@@ -33,7 +31,6 @@ def loadgeant4(env, OSENV) :
 				validg4incs.append('-I'+ss);
 
 	# geant4 had this additional flag.
-	# validg4incs.append('-std=c++98');
 	env.Append(CXXFLAGS  = validg4incs)
 
 	## Libraries
@@ -41,7 +38,6 @@ def loadgeant4(env, OSENV) :
 	# some systems the library order comes out not correct
 	# and LINKFLAGS always come before LIBS
 	# So we use the geant4-config and strip the path and the libs independently
-	# Also, we strip out CLHEP from the libraries
 	g4libs =  os.popen('$G4INSTALL/bin/geant4-config --libs').readline()
 
 	# splits list based on -l
@@ -66,23 +62,18 @@ def loadgeant4(env, OSENV) :
 
 	if env['PLATFORM'] == 'posix':
 		env.Append(CXXFLAGS='-I/usr/include/GL')
-		# env.Append(CXXFLAGS='-DG4VIS_USE_OPENGLX')  # no more needed? 1/13/23
 		validg4libs.append('GL')
-		# manually adding G4zlib
-		# looks like geant4-config does not provide these
-		#validg4libs.append('G4zlib')
 		validg4libs.append('pthread')
-		#validg4libs.append('G4ptl')
-		#validg4libs.append('G4tasking')
+		validg4libs.append('G4ptl')
+		validg4libs.append('G4tasking')
 
 	if env['PLATFORM'] == 'darwin':
-	#	env.Append(CXXFLAGS='-I/System/Library/Frameworks/OpenGL.framework/Headers')
-	#	env.Append(LINKFLAGS = '-L/System/Library/Frameworks/OpenGL.framework/Libraries/')
+		env.Append(CXXFLAGS='-I/System/Library/Frameworks/OpenGL.framework/Headers')
+		env.Append(LINKFLAGS = '-L/System/Library/Frameworks/OpenGL.framework/Libraries/')
 		validg4libs.append('GL')
-		# looks like geant4-config does not provide these
 		validg4libs.append('pthread')
-		#validg4libs.append('G4ptl.0')
-		#validg4libs.append('G4tasking')
+		validg4libs.append('G4ptl')
+		validg4libs.append('G4tasking')
 
 	if env['PLATFORM'] == 'win32':
 		validg4libs.append('glu32')
