ddf7ed42e2759b11b2239f8fa36d9264154d61c6
Maurizio Ungaro <ungaro@jlab.org>
2023-02-17 16:50:38 -0500

    loadroot does not rely on environment anymore

--- loadroot.py
+++ loadroot.py
@@ -2,48 +2,36 @@ from util import check_if_dir_exist
 import os
 import re
 
-def loadroot(env, OSENV) :
 
-	ROOTSYS = OSENV['ROOTSYS']
-	check_if_dir_exist('ROOTSYS', ROOTSYS)
+def loadroot(env, OSENV):
+	BINROOT = os.popen('root-config --bindir').readline().strip()
 
 	inc_path = []
 
+	# includes
 	if env['PLATFORM'] == 'posix' or env['PLATFORM'] == 'darwin':
-		inc_path = os.popen('$ROOTSYS/bin/root-config --incdir').readline().rstrip()
+		inc_path = os.popen(BINROOT+ '/root-config --incdir').readline().rstrip()
 
-	# on Windows there is no script
-	# Have to do it by hand
-	if env['PLATFORM'] == 'win32':
-		inc_path = ROOTSYS + "\include"
+	env.Append(CXXFLAGS=env['INCPREFIX'] + inc_path)
 
-	env.Append(CXXFLAGS = env['INCPREFIX'] + inc_path)
-
+	# libraries and paths
 	rootlibs = []
 	root_config_libs = []
 	if env['PLATFORM'] == 'posix' or env['PLATFORM'] == 'darwin':
 		# manual root_config_libs
-		#root_config_libs = ['-lCore', '-lRIO', '-lTree', '-lHist']
-		root_config_libs = os.popen('$ROOTSYS/bin/root-config --glibs').readline().rstrip().split()
-		root_libpath     = os.popen('$ROOTSYS/bin/root-config --libdir').readline().rstrip().split()
-
-	# on Windows there is no script
-	# Have to do it by hand
-	# Do a showenv on Linux/Darwin and copy here
-	if env['PLATFORM'] == 'win32':
-		rootlibs = ['libCore', 'libCint', 'libRIO', 'libNet', 'libHist', 'libGraf', 'libGraf3d', 'libGpad', 'libTree', 'libRint', 'libPostscript', 'libMatrix', 'libPhysics', 'libMathCore', 'libThread', 'libGui']
+		# root_config_libs = ['-lCore', '-lRIO', '-lTree', '-lHist']
+		root_config_libs = os.popen(BINROOT + '/root-config --glibs').readline().rstrip().split()
+		root_libpath = os.popen(BINROOT + '/root-config --libdir').readline().rstrip().split()
 
 	# This filters out all the "-lxxx" parts of the root_config_libs
-	rootlibs += [ x for x in root_config_libs if re.match('-l',x) ]
-
+	rootlibs += [x for x in root_config_libs if re.match('-l', x)]
 	# This grabs the "-L" part of the root_config_libs
-	env.Append(LIBPATH = root_libpath )
+	env.Append(LIBPATH=root_libpath)
 
 	if env['SHOWENV'] == "1":
-		print ("\n > Loading ROOT software from ", ROOTSYS)
-		print ("   ROOT include flags: ",  inc_path)
-		print ("   ROOT libraries path: ", root_libpath)
-		print ("   ROOT libraries: ",      rootlibs)
+		print("\n > Loading ROOT software from as directed by root-config in", BINROOT)
+		print("   ROOT include flags: ", inc_path)
+		print("   ROOT libraries path: ", root_libpath)
+		print("   ROOT libraries: ", rootlibs)
 
-	env.Append(LIBS = rootlibs)
-
+	env.Append(LIBS=rootlibs)
